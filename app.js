697e9890f2fd9b3b5fd21dfbe3326cc4ae7a3ac9
DanielE <daniel6692@walla.com>
2015-10-16 15:11:59 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var http = require('http');
+
+var app = express();
+var server = http.Server(app);
+server.listen(3000);
+
+
+app.get('/', function(req, res){
+    res.;
+});
